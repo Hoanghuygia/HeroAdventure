c586dd3e145aaa6e978d1c8c84d6309a6a33b29d
Hoanghuygia <hoanghuy051230@gmail.com>
2023-05-09 22:56:30 +0700

    fix pit bug(cancel 2nd active time)

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -9,6 +9,8 @@ public class EventHandler {
 
     int previousEventX, previousEventY;
     boolean canTouchEvent = true;
+    Pit pit1 = new Pit(27, 16);
+    Pit pit2 = new Pit(23, 19);
 
     public EventHandler(GamePanel gp) {
         this.gp = gp;
@@ -50,8 +52,8 @@ public class EventHandler {
         }
 
         if (canTouchEvent) {
-            if (hit(0,27, 16, "right")) {damagePit( gp.dialogueState);}
-//            else if (hit(0,23, 19, "any")) {damagePit( gp.dialogueState);}
+            if (hitPit(0,pit1, "right")) {damagePit( pit1, gp.dialogueState);}
+            else if (hitPit(0,pit2, "any")) {damagePit( pit2, gp.dialogueState);}
             else if (hit(0,23, 12, "up")) {healingPool( gp.dialogueState);}
             else if(hit(0, 23, 24, "any")) {teleport(1, 12, 13);}
             else if(hit(1, 12, 13, "any")) {teleport(0, 23, 24);}
@@ -83,13 +85,41 @@ public class EventHandler {
 
         return hit;
     }
+    public boolean hitPit(int map, Pit pit, String reqDirection) {
+        boolean hit = false;
+        if(pit.pitOn){
+            if(map == gp.currentMap){
+                gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
+                gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
+                eventRect[map][pit.x][pit.y].x = pit.x * gp.tileSize + eventRect[map][pit.x][pit.y].x;
+                eventRect[map][pit.x][pit.y].y = pit.y * gp.tileSize + eventRect[map][pit.x][pit.y].y;
 
-    public void damagePit( int gameState) {
+                if (gp.player.solidArea.intersects(eventRect[map][pit.x][pit.y]) && !eventRect[map][pit.x][pit.y].eventDone) {//cần sửa lại cái eventDone này thành true không ta
+                    if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
+                        hit = true;
+
+                        previousEventX = gp.player.worldX;
+                        previousEventY = gp.player.worldY;
+                    }
+                }
+
+                gp.player.solidArea.x = gp.player.solidAreaDefaultX;
+                gp.player.solidArea.y = gp.player.solidAreaDefaultY;
+                eventRect[map][pit.x][pit.y].x = eventRect[map][pit.x][pit.y].eventRectDefaultX;
+                eventRect[map][pit.x][pit.y].y = eventRect[map][pit.x][pit.y].eventRectDefaultY;
+            }
+        }
+
+        return hit;
+    }
+
+    public void damagePit( Pit pit,int gameState) {
         gp.gameState = gameState;
         gp.playSE(6);
         gp.ui.currentDialogue = "You fall into a pit!";
         gp.player.life -= 1;
         canTouchEvent = false;
+        pit.pitOn = false;
     }
 
     public void healingPool( int gameState) {
---
+++ src/main/Pit.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Pit {
+    int x, y;
+    int map;
+    boolean pitOn = true;
+    public Pit(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
