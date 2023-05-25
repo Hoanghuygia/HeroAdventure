68e14145ca23e40cfb4713bcbfe108e23d39ca1e
HTNT0117 <ngoctram.bie.u@gmail.com>
2023-05-25 13:27:43 +0700

    Tile and map

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -44,8 +44,8 @@ public class Player extends Entity {
     }
 
     public void setDefaultValues() {
-        worldX = gp.tileSize * 23;
-        worldY = gp.tileSize * 21;
+        worldX = gp.tileSize * 40;
+        worldY = gp.tileSize * 18;
 //        worldX = gp.tileSize * 12;
 //        worldY = gp.tileSize * 13;
         defaultSpeed = 4;
